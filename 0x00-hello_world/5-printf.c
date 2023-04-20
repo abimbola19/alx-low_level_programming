823c65302098f347b4adad52d02e2471c2be4b83
abimbola19 <abimbolaonafowora@gmail.com>
2023-04-20 15:52:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - program to a line with printf
+ * main - program to a line with printf function
  * Return: 0 (Success)
  */
 int main(void){
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
