4201bf59556bbeaf25e7c38c3695f8449b8ee3db
abimbola19 <abimbolaonafowora@gmail.com>
2023-04-26 15:02:18 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * times_table - Display multiply table.
+ */
+
+void times_table(void)
+{
+	int i, j, n, c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		c = 0;
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
+			if (n <= 9 && i * (j + 1) <= 9)
+			{
+				_putchar(n + '0');
+				if (c != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				if (n / 10 == 0)
+				{
+					_putchar(n + '0');
+				}
+				else
+				{
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+				}
+				if (c != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			c++;
+		}
+		_putchar('\n');
+	}
+}
