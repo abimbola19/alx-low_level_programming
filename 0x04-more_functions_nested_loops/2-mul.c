c19c88cf5e340f6cda9b3dfd8f76c356ea1bfca6
abimbola19 <abimbolaonafowora@gmail.com>
2023-04-28 15:11:33 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Check digit
+ * @a: First number
+ * @b: second number
+ *
+ * Return: Multiplication of two numbers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
